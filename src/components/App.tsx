f65b9e4237e1e0501b2a8c777ea61a504eddcc2c
Quentin Smith <quentin@mit.edu>
2024-05-12 19:14:42 -0400

    Embed a link in the App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "./Layout";
 import {Counter} from "./counter";
+import { Link } from "@mfng/core/client";
 
 export function App() {
   return (
     <Layout>
       <div>Hello world</div>
       <Counter />
+      <Link to={{pathname: "/foo"}}>Click me</Link>
     </Layout>
   );
 }
